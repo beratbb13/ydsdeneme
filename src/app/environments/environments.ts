ea73db39474a157f9702a810b2953652501a2020
kubra.goktas@consultant.turkcell.com.tr <kubra.goktas@consultant.turkcell.com.tr>
2023-09-25 13:51:31 +0300

    error resolved

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://demo.bussion.com',
+    baseUrl: 'https://demo.bussion.com',
     // baseUrl:"http://192.168.42.164",
 
-    imageUrl: 'http://demo.bussion.com',
+    imageUrl: 'https://demo.bussion.com',
 };
