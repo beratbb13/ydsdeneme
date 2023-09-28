369fd730b16ad201be42cd535d8557effc875bb9
Sedanur Babir <80528795+SedanurBabir@users.noreply.github.com>
2023-09-28 10:27:29 +0300

    before deneme pages updated , pagination bottom fixed

--- src/app/views/pages/before-deneme/before-deneme.component.ts
+++ src/app/views/pages/before-deneme/before-deneme.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./before-deneme.component.css']
 })
 export class BeforeDenemeComponent {
+  selectedExam: string = ''; 
+  exams = [
+    { name: 'YDS', text: 'lorem ipsum dolar sit amet', img: '/assets/icons/yds.png' },
+    { name: 'KPSS', text: 'lorem ipsum dolar sit amet', img: '/assets/icons/kpss.png' },
+    { name: 'YGS', text: 'lorem ipsum dolar sit amet', img: '/assets/icons/ygs.png' },
+    { name: 'ALES', text: 'lorem ipsum dolar sit amet', img: '/assets/icons/ales.png' },
+    { name: 'LGS', text: 'lorem ipsum dolar sit amet', img: '/assets/icons/lgs.png' },
 
+  ]
+  
 }
